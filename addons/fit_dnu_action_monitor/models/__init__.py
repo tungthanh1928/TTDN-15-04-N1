392da807f24d9555ad33a4818a80b6b27de9f1a0
TTDN <ttdn@gmail.com>
2025-01-16 06:31:30 +0000

    ?

--- addons/fit_dnu_action_monitor/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import student_log_action
-from . import ca_hoc
-from . import log_sinh_vien_ca_hoc
--- addons/fit_dnu_crm/controllers/main.py
+++
@@ -1,7 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class ChartController(http.Controller):
-    @http.route('/chart', type='http', auth='user')
-    def chart(self, **kw):
-        return request.render('your_module.chart_template')
